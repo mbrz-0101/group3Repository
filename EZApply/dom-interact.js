012a5bcceefd1a99502ac692a2a146c19f302559
Matt Brzowski <5thhand@gmail.com>
2018-02-12 20:59:03 -0700

    fix errors, styles

--- EZApply/dom-interact.js
+++ EZApply/dom-interact.js
@@ -1,6 +1,21 @@
-function highlightInputField {
-  document.getElementsByTagName('input').style.backgroundColor = "blue";
-}
+// function myFunction() {
+//   var attr = document.createAttribute("class");
+//   attr.value = "democlass";
+//   var h = document.getElementsByTagName("H1")[0];
+//   h.setAttributeNode(attr); 
+// }
 
-var inputs = document.getElementsByTagName('input');
-inputs.forEach(function(inputField) {inputField.backgroundColor = "blue"});
+
+// function myFunction() {
+//   var input = document.getElementsByTagName("input")[0];
+//   var att = document.createAttribute("class");
+//   att.value = "input";
+//   input.setAttributeNode(att);
+// }
+
+document.input.style.backgroundColor = "red";
+
+
+// var inputs = document.getElementsByClassName('type');
+// console.log(inputs);
+// inputs.forEach(function(inputField) {inputField.style.backgroundColor = "blue"; !important});
