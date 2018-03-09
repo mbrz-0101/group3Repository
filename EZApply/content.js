d66256ae8eacc53b789dab3c8bb40d423df0e6a8
Matt Brzowski <5thhand@gmail.com>
2018-03-09 09:29:45 -0700

    remove event listeners from popup and add autofill in content

---
+++ EZApply/content.js
@@ -0,0 +1,73 @@
+console.log("This content script is working");
+
+function fillInForm() {
+  let $inputs = $("input");
+  console.log($inputs);
+
+  for (elt in $inputs) {
+
+    // First name
+    if ($inputs[elt].name && ($inputs[elt].name === "firstName" || $inputs[elt].name.includes("firstName") || $inputs[elt].name.includes("FirstName"))) {
+      $($inputs[elt]).val("Matt");
+    }
+
+    // Last name
+    if ($inputs[elt].name && ($inputs[elt].name === "lastName")) {
+      $($inputs[elt]).val("Brzowski");
+    }
+
+    // Email
+    if ($inputs[elt].name && ($inputs[elt].name === "email")) {
+      $($inputs[elt]).val("my@email.online");
+    }
+    
+    // Address
+
+    // Address 2
+
+    // City
+
+    // State
+
+    // Zipcode
+
+
+    // Job Title 1
+
+    // Job Company 1
+
+    // Job Duties 1
+
+    // Job Phone 1
+    
+    // Job Supervisor 1 
+
+    // Job Address 1
+    
+    // Job Adress 2
+
+    // Job City 1
+
+    // Job State 1
+
+    // Job Zip 1
+
+
+    // School Name 1
+
+    // Degree 
+
+    // Major
+
+    // GPA
+
+
+    // Certification Title 1
+    
+    // Certification Detail 1
+
+
+
+    // Request additional fields button?
+  }
+}
--- EZApply/popup.js
+++ EZApply/popup.js
@@ -5,11 +5,6 @@ function executeTheseFunctions(e) {
     {
       code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].style.border = 'cadetblue 4px solid'}"
     });
-  // add event listeners to input tags
-  chrome.tabs.executeScript(null,
-    {
-      code: "var inputs = document.getElementsByTagName('input'); for (var i = 0; i < inputs.length; i++) {inputs[i].addEventListener('click', contentScriptFunction)}"
-    })
   chrome.tabs.executeScript(null,
     {
       code: "fillInForm();"
